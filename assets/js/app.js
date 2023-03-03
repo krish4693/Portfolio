63ed4746fbe0d2f340873d959a2ca355a5fc5a1e
Krishnanand U <krishnanandunnikrishnan@gmail.com>
2023-03-03 14:28:26 +0530

    added footer,styled scroll-up button and fixed navigation postioning

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
-                                                        
+const scrollUp=document.querySelector('#scroll-up')
+
+scrollUp.addEventListener('click',()=>{
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior: "smooth"
+    })
+})
+
+const navigationHeight=document.querySelector('.primary-navigation').offsetHeight;
+// console.log(navigationHeight);
+
+document.documentElement.style.setProperty('--scroll-padding',navigationHeight+'px');    
